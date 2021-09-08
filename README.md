51ebafd6a5773e3ae411a98a193fb8abe4b77948
Kristian Bonnici <kristiandaaniel@gmail.com>
2021-09-09 00:35:37 +0200

    First build

---
+++ README.md
@@ -0,0 +1,43 @@
+<h1 align="center">Portfolio Optimizer</h1>
+
+<p align="center">Portfolio optimization web app.</p>
+
+## Live app
+
+You can find a running system to use [here](#) (soming soon).
+
+## Introduction
+`portfolio-optimization-web-app` is a web application for generating optimal portfolios based on Sharpe Ratio.
+
+
+## Screenshots
+
+![Screenshot1](/static/images/Screenshot1.png)
+![Screenshot2](/static/images/Screenshot2.png)
+
+## Built With
+
+**Back End:**
+- **[Flask](https://flask.palletsprojects.com/en/2.0.x/)** - The web application framework used
+- **[Python](https://www.python.org/)** - Backend language used
+- **[Optifolio [PyPI Package]](https://github.com/kristianbonnici/optifolio)** - My package used for portfolio optimization
+
+**Front End:**
+- **HTML** - used to design the front-end portion
+- **CSS** - used to style the front-end portion
+- **JavaScript** - used to create some interactivity
+
+
+## Author
+
+**Kristian Bonnici**
+
+- [Profile](https://github.com/kristianbonnici)
+- [Email](mailto:kristian.bonnici@aalto.fi)
+- [Website](https://kristianbonnici.github.io/)
+
+## 🤝 Support
+
+Contributions, issues, and feature requests are welcome!
+
+Give a ⭐️ if you like this project!
--- portfoliodash/dashboard.py
+++ portfoliodash/dashboard.py
@@ -1,13 +1,13 @@
-
 from flask import (
-    Blueprint, flash, g, redirect, render_template, request, session, url_for
+    Blueprint, flash, redirect, render_template,
+    request, url_for
 )
 import json
 from datetime import datetime
-import scipy
 import pandas_datareader.data as web
 from optifolio import PortfolioOptimizer
 from bokeh.embed import components
+import bokeh
 
 # Blueprint
 #      * named 'dashboard'
@@ -20,23 +20,48 @@ bp = Blueprint('dashboard', __name__, url_prefix='/')
 @bp.route('/', methods=['GET', 'POST'])  # homepage
 def index():
     if request.method == 'GET':
-        return render_template('index.html')
+        return render_template('index.html', is_index=True)
     else:
+        bokeh_version = bokeh.__version__
 
         data_dict = request.form.to_dict(flat=False)
         start_date = datetime.strptime(data_dict['start'][0], '%Y-%m-%d')
         end_date = datetime.strptime(data_dict['end'][0], '%Y-%m-%d')
-        min_ret = float(data_dict['min-ret'][0])/100
+        min_ret = float(data_dict['min-ret'][0]) / 100
         tickers = [x.strip() for x in data_dict['tickers'][0].split(',')]
         benchmark_ticker = data_dict['benchmark'][0].strip()
-        print(benchmark_ticker)
 
-        data = web.DataReader(tickers, 'yahoo',
-                              start=start_date, end=end_date)['Adj Close']
+        if '' in tickers:
+            flash("""Invalid tickers.""")
+            return redirect(url_for('dashboard.index'))
+        if start_date >= end_date:
+            flash("""The start date must be before the end date.""")
+            return redirect(url_for('dashboard.index'))
+
+        try:
+            data = web.DataReader(tickers, 'yahoo',
+                                  start=start_date, end=end_date)['Adj Close']
+            print(data)
+        except:
+            flash("""Failed loading data from the Yahoo Finance API.""")
+            return redirect(url_for('dashboard.index'))
+
+        for col in data:
+            na_values = data[col].isna().sum()
+            values = len(data[col])
+            if values == na_values:
+                flash("""ERROR: There is no data for {}.""".format(col))
+                return redirect(url_for('dashboard.index'))
+            if na_values != 0:
+                flash("""{col} includes {na_val} NaN values ({na_val}/{val}).
+                """.format(col=col, na_val=na_values, val=values))
 
         model = PortfolioOptimizer().fit(data, min_ret=min_ret)
-        p1 = model.plot_efficient_frontier(output=None, width=800, height=410,
-                                           border_fill_color='#28283B', toolbar=True)
+        p1 = model.plot_efficient_frontier(output=None,
+                                           width=800,
+                                           height=410,
+                                           border_fill_color='#28283B',
+                                           toolbar=True)
         p1.sizing_mode = "scale_width"
         script1, div1 = components(p1)
 
@@ -46,10 +71,20 @@ def index():
         script2, div2 = components(p2)
 
         if benchmark_ticker == '':
-            benchmark_ticker = None
+            benchmark = None
         else:
-            benchmark = web.DataReader(
-                [benchmark_ticker], 'yahoo', start=start_date, end=end_date)['Adj Close']
+            benchmark = web.DataReader([benchmark_ticker], 'yahoo',
+                                       start=start_date,
+                                       end=end_date)['Adj Close']
+            if benchmark_ticker == '^GSPC':
+                benchmark.columns = ['S&P 500 (^GSPC)']
+            elif benchmark_ticker == '^IXIC':
+                benchmark.columns = ['NASDAQ Composite (^IXIC)']
+            elif benchmark_ticker == '^DJI':
+                benchmark.columns = ['Dow Jones Industrial Average (^DJI)']
+            elif benchmark_ticker == '^RUT':
+                benchmark.columns = ['Russell 2000 (^RUT)']
+
         p3 = model.plot_cumulative_return(output=None, width=1200, height=300,
                                           benchmark_data=benchmark,
                                           border_fill_color='#28283B',
@@ -57,11 +92,38 @@ def index():
         p3.sizing_mode = "scale_width"
         script3, div3 = components(p3)
 
+        ret = model.ret
+        rf_ret = model.rf_ret
+        vol = model.vol
+        sharpe = model.sharpe
+        weights = model.weights
+        stock_weights = model.stock_weights
+        stock_names = model.stock_names
+
+        print("Weights:", weights)
+        print("Stock weights:", stock_weights)
+
         # open/create JSON file & save user's input
         with open('portfolio.json', 'w') as data_file:
             json.dump(data_dict, data_file)  # save url to JSON
 
-        return render_template('index.html', data=data_dict,
+        return render_template('index.html', is_index=True,
+                               data=data_dict,
+                               bokeh_version=bokeh_version,
                                script1=script1, div1=div1,
                                script2=script2, div2=div2,
-                               script3=script3, div3=div3)
+                               script3=script3, div3=div3,
+                               stock_names=stock_names,
+                               stock_weights=stock_weights * 100,
+                               ret=ret * 100, rf_ret=rf_ret * 100,
+                               vol=vol * 100, sharpe=sharpe)
+
+
+@bp.route('/info')
+def info():
+    return render_template('info.html', is_index=False)
+
+
+@bp.errorhandler(404)
+def page_not_found():
+    return 404
